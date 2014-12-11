a77b1944b90561ebf5fbe14f3352cdd43d6b3f96
cryptncyrix <cyrix@t-online.de>
2014-12-11 09:34:06 +0100

    Update 2014_12_04_143642_acl_ressource.php

--- Database/migrations/2014_12_04_143642_acl_ressource.php
+++ Database/migrations/2014_12_04_143642_acl_ressource.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AclRessource extends Migration {
+class AclResource extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class AclRessource extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('ressources', function(Blueprint $table)
+		Schema::create('resources', function(Blueprint $table)
 		{
 			$table->increments('id')->unsigned();
                         $table->string('name')->unique();
@@ -28,7 +28,7 @@ class AclRessource extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('ressources');
+		Schema::drop('resources');
 	}
 
 }
