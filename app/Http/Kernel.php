f6cc1beb551c4cf634a94e7c4f3519adc04f5886
cryptncyrix <cyrix@t-online.de>
2014-12-11 09:35:55 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
-    'acl' => 'App\Http\Middleware\Acl',
+    		'acl' => 'App\Http\Middleware\Acl',
 	];
 
 }
