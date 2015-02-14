5faf8f2b55842b1c592ad6097581840e213c2d5a
cryptncyrix <cyrix@t-online.de>
2015-02-14 10:14:53 +0100

    Update 2014_12_04_152108_user_has_acl_role.php

--- Database/migrations/2014_12_04_152108_user_has_acl_role.php
+++ Database/migrations/2014_12_04_152108_user_has_acl_role.php
@@ -14,10 +14,11 @@ class UserHasAclRole extends Migration {
 	{
 		Schema::create('users_roles', function(Blueprint $table)
 		{
-		        $table->integer('user_id')->unsigned();
+			$table->integer('user_id')->unsigned();
                         $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		        $table->integer('role_id')->unsigned();
+			$table->integer('role_id')->unsigned();
                         $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+                        $table->unique(array('user_id', 'role_id'), 'userid_roleid');
 			$table->timestamps();
 		});
 	}
