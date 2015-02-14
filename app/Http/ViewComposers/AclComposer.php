d7e2773d63fb086e688a04a8836fd2fab696828e
cryptncyrix <cyrix@t-online.de>
2015-02-14 10:11:23 +0100

    Create AclComposer.php

---
+++ app/Http/ViewComposers/AclComposer.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\ViewComposers;
+
+use App\Helper\AclHelper;
+use Illuminate\Contracts\View\View;
+
+/**
+ * Description of MaterialComposer
+ *
+ * @author Christian
+ */
+class AclComposer {
+    
+    private $aclHelper;
+    
+    public function __construct(AclHelper $aclHelper)
+    {
+        $this->aclHelper = $aclHelper;
+    }
+    
+    public function compose(View $view)
+    {
+        $view->with('acl', $this->aclHelper->getAllTrueResources());
+    }
+    
+}
