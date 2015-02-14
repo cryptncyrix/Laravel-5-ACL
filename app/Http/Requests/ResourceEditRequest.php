7576b864f3acf59f025fce53faf7a9193b3de434
cryptncyrix <cyrix@t-online.de>
2015-02-14 23:36:48 +0100

    Create ResourceEditRequest.php

---
+++ app/Http/Requests/ResourceEditRequest.php
@@ -0,0 +1,36 @@
+<?php namespace App\Http\Requests;
+
+class ResourceEditRequest extends Request {
+    
+        /**
+         * The URI to redirect to if validation fails
+         *
+         * @var string
+         */
+        protected $redirect = 'acl/resource';    
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+                        '_id'                   => 'required|integer',
+			'name'                  => 'required|max:200',
+			'rights'                => 'required|boolean'
+		];
+	}
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+}
