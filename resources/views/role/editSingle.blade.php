c176172570d9bfd510c7fb47680e9c9374bd655e
cryptncyrix <cyrix@t-online.de>
2015-02-14 11:16:34 +0100

    Create editSingle.blade.php

---
+++ resources/views/role/editSingle.blade.php
@@ -0,0 +1,57 @@
+@extends('app')
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+                        <h4>Rolle Bearbeiten - {!! $dbRole->name !!}</h4>
+
+			<div class="well well-lg">
+
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+                                        {!! Form::open(['route' => ['acl.doEditRole'], 'class' => 'form-horizontal']) !!}
+                                        
+                                        {!! Form::hidden('_id', $dbRole->id) !!}
+                                        
+                                        <div class="form-group">
+                                            {!! Form::label('name', 'Rolle', ['class' => 'col-sm-4 control-label' ]) !!}
+                                            <div class="col-sm-6">{!! Form::text('name', $dbRole->name, ['class' => 'form-control',
+                                                                                                         'placeholder' => 'Rolle']) !!}</div>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            {!! Form::label('rights', 'Standardrecht', ['class' => 'col-sm-4 control-label' ]) !!}
+                                            <div class="col-sm-6"> {!! Form::radio('rights', 0, ($dbRole->default_access == 0) ? true : false ) !!} Verbieten </div>
+                                            <div class="col-sm-6"> {!! Form::radio('rights', 1, ($dbRole->default_access == 1) ? true : false ) !!} Erlauben </div>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <div class='col-sm-6 col-sm-offset-4'>
+                                                <div class="col-sm-4">{!! Form::submit('Rolle bearbeiten', ['class' => 'btn btn-primary']) !!}</div>
+                                            </div>
+                                            
+                                        </div>
+                                        
+                                        @if (Session::get('msg'))
+                                            <div class="alert alert-success">{{ Session::get('msg') }}</div>
+                                        @endif
+                                        
+                                        {!! Form::close() !!}
+					
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
