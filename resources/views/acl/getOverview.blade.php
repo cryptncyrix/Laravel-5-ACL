fcce0d0ca073e5ac8963fba8af1fbac7cce8160b
cryptncyrix <cyrix@t-online.de>
2015-02-25 22:50:30 +0100

    Update getOverview.blade.php

--- resources/views/acl/getOverview.blade.php
+++ resources/views/acl/getOverview.blade.php
@@ -4,21 +4,24 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-                        <h4>Ãœbersicht</h4>
-			<div class="well well-lg">
+                        <h4>Übersicht</h4>
+			<div class="well well-lg text-center">
                             
+                            @if(hasResource('acl.allRoles')) 
                             <div class="col-sm-4">
                                 <a href="{!! route('acl.allRoles') !!}"> Rollen </a>
                             </div>
-
+                            @endif
+                            @if(hasResource('acl.allResources'))
                             <div class="col-sm-4">
                                 <a href="{!! route('acl.allResources') !!}"> Resourcen </a>
-                            </div>  
-                            
+                            </div> 
+                            @endif
+                            @if(hasResource('user.all'))
                             <div class="col-sm-4">
                                 <a href="{!! route('user.all') !!}"> User </a>
                             </div> 
-
+                            @endif
 			</div>
 		</div>
 	</div>
