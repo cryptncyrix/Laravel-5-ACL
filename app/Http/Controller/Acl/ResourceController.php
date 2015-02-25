293e87760c921f46bb08123bb18aca1227160550
cryptncyrix <cyrix@t-online.de>
2015-02-25 22:30:47 +0100

    Update ResourceController.php

--- app/Http/Controller/Acl/ResourceController.php
+++ app/Http/Controller/Acl/ResourceController.php
@@ -5,8 +5,6 @@ use App\Database\Models\Resource;
 use App\Http\Requests\ResourceRequest;
 use App\Http\Requests\ResourceEditRequest;
 
-
-
 /**
  * @Controller(prefix="acl")
  * @Middleware("acl")
@@ -14,89 +12,102 @@ use App\Http\Requests\ResourceEditRequest;
 
 class ResourceController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Resource Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
-    
-       protected $resource;
+    /*
+    |--------------------------------------------------------------------------
+    | Resource Controller
+    |--------------------------------------------------------------------------
+    */
 
-       /**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  Role  $resource
-	 * @return void
-	 */
-	public function __construct(Resource $resource)
-	{
-            $this->resource = $resource;
-	}
+    /**
+     *
+     * @var type 
+     */
+    protected $resource;
 
-	/**
-	 * Show the application login form.
-         * @GET("resource", as="acl.resource")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getResource()
-	{
-		return view('resource.create');
-	}
+   /**
+     * Create a new authentication controller instance.
+     *
+     * @param  Role  $resource
+     * @return void
+     */
+    public function __construct(Resource $resource)
+    {
+        $this->resource = $resource;
+    }
 
-	/**
-	 * Handle a login request to the application.
-         * @POST("resource", as="acl.doResource")
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function postPost(ResourceRequest $request)
-	{
-            $this->resource->insertNew($request->get('name'), $request->get('rights'));
-            return redirect()->route('acl.resource')->with('msg', 
-                                     'Resource erfolgreich angelegt.');
-	}
-        
-        /**
-	 * Show the application login form.
-         * @GET("resource/edit/{name}", as="acl.editResource")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function getEditSingle($name)
-        {
-            $dbResource = $this->resource->findByName($name);
-            return view('resource.editSingle', compact('dbResource')); 
-        }
+    /**
+     * Show the application login form.
+     * @GET("resource", as="acl.resource")
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getResource()
+    {
+            return view('resource.create');
+    }
 
-        /**
-	 * Show the application login form.
-         * @POST("resource/edit", as="acl.doEditResource")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function postEditSingle(ResourceEditRequest $request)
-        {
-            $this->resource->editById($request);
-            return redirect()->route('acl.resource')->with('msg', 
-                                     'Resource erfolgreich bearbeitet.');
-        }
-        
-        /**
-	 * Show the application login form.
-         * @GET("resource/all", as="acl.allResources")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function getAll()
-        {
-            $dbResource = $this->resource->getAll(1);
-            return view('resource.all', compact('dbResource'));
-        }
+    /**
+     * Handle a login request to the application.
+     * @POST("resource", as="acl.doResource")
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postPost(ResourceRequest $request)
+    {
+        $this->resource->insertNew($request->get('name'), $request->get('rights'));
+        return redirect()->route('acl.resource')->with('msg', 
+                                 'Resource erfolgreich angelegt.');
+    }
+
+    /**
+     * Show the application login form.
+     * @GET("resource/edit/{name}", as="acl.editResource")
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getEditSingle($name)
+    {
+        $dbResource = $this->resource->findByName($name);
+        return view('resource.editSingle', compact('dbResource')); 
+    }
+
+    /**
+     * Show the application login form.
+     * @POST("resource/edit", as="acl.doEditResource")
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postEditSingle(ResourceEditRequest $request)
+    {
+        $this->resource->editById($request);
+        return redirect()->route('acl.resource')->with('msg', 
+                                 'Resource erfolgreich bearbeitet.');
+    }
+
+    /**
+     * Show the application login form.
+     * @GET("resource/all", as="acl.allResources")
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAll()
+    {
+        $dbResource = $this->resource->getAll(25);
+        return view('resource.all', compact('dbResource'));
+    }
+
+    /**
+     * Show the application login form.
+     * @GET("resource/route", as="acl.routeResource")
+     *
+     * @return \Illuminate\Http\Response
+     */   
+
+    public function setAllRoutesAsResources()
+    {
+        $this->resource->setAllNewRoutesToResources(\Route::getRoutes());    
+        return redirect()->route('acl.allResources')->with('msg', 
+                                 'Resource erfolgreich bearbeitet.');
+    }
 }
