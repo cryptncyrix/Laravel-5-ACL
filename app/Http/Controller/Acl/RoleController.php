e300c8b40abe9a4767238cec40a6c79735eb815e
cryptncyrix <cyrix@t-online.de>
2015-02-25 22:32:33 +0100

    Update RoleController.php

--- app/Http/Controller/Acl/RoleController.php
+++ app/Http/Controller/Acl/RoleController.php
@@ -13,90 +13,90 @@ use App\Http\Requests\RoleEditRequest;
 
 class RoleController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Acl Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Acl Controller
+    |--------------------------------------------------------------------------
+    */
     
-       protected $role;
+    /**
+     *
+     * @var type 
+     */
+    
+    protected $role;
 
-       /**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  Role  $role
-	 * @return void
-	 */
-	public function __construct(Role $role)
-	{
-            $this->role = $role;
-	}
+    /**
+      * Create a new authentication controller instance.
+      *
+      * @param  Role  $role
+      * @return void
+      */
+     public function __construct(Role $role)
+     {
+         $this->role = $role;
+     }
 
-	/**
-	 * Show the application login form.
-         * @GET("role", as="acl.role")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getRoles()
-	{
-		return view('role.create');
-	}
+     /**
+      * Show the application login form.
+      * @GET("role", as="acl.role")
+      *
+      * @return \Illuminate\Http\Response
+      */
+     public function getRoles()
+     {
+        return view('role.create');
+     }
 
-	/**
-	 * Handle a login request to the application.
-         * @POST("role", as="acl.doRole")
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function postRoles(RoleRequest $request)
-	{
-            $this->role->insertNew($request->get('name'), $request->get('rights'));
-            return redirect()->route('acl.role')->with('msg', 
-                                     'Rolle erfolgreich angelegt.');
-	}
-        
-        /**
-	 * Show the application login form.
-         * @GET("role/edit/{name}", as="acl.editRole")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function getEditSingle($name)
-        {
-            $dbRole = $this->role->findByName($name);
-            return view('role.editSingle', compact('dbRole')); 
-        }
+     /**
+      * Handle a login request to the application.
+      * @POST("role", as="acl.doRole")
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @return \Illuminate\Http\Response
+      */
+     public function postRoles(RoleRequest $request)
+     {
+         $this->role->insertNew($request->get('name'), $request->get('rights'));
+         return redirect()->route('acl.role')->with('msg', 
+                                  'Rolle erfolgreich angelegt.');
+     }
 
-        /**
-	 * Show the application login form.
-         * @POST("role/edit", as="acl.doEditRole")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function postEditSingle(RoleEditRequest $request)
-        {
-            $this->role->editById($request);
-            return redirect()->route('acl.role')->with('msg', 
-                                     'Rolle erfolgreich bearbeitet.');
-        }
-        
-        /**
-	 * Show the application login form.
-         * @GET("role/all", as="acl.allRoles")
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-        public function getAll()
-        {
-            $dbRole = $this->role->getAll(1);
-            return view('role.all', compact('dbRole'));
-        }
+     /**
+      * Show the application login form.
+      * @GET("role/edit/{name}", as="acl.editRole")
+      *
+      * @return \Illuminate\Http\Response
+      */
+     public function getEditSingle($name)
+     {
+         $dbRole = $this->role->findByName($name);
+         return view('role.editSingle', compact('dbRole')); 
+     }
+
+     /**
+      * Show the application login form.
+      * @POST("role/edit", as="acl.doEditRole")
+      *
+      * @return \Illuminate\Http\Response
+      */
+     public function postEditSingle(RoleEditRequest $request)
+     {
+         $this->role->editById($request);
+         return redirect()->route('acl.role')->with('msg', 
+                                  'Rolle erfolgreich bearbeitet.');
+     }
+
+     /**
+      * Show the application login form.
+      * @GET("role/all", as="acl.allRoles")
+      *
+      * @return \Illuminate\Http\Response
+      */
+     public function getAll()
+     {
+         $dbRole = $this->role->getAll(1);
+         return view('role.all', compact('dbRole'));
+     }
 
 }
