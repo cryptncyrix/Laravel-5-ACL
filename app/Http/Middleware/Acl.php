17ab9d56cbd86596438e8caca69f915fc333fa71
cryptncyrix <cyrix@t-online.de>
2015-02-25 22:38:28 +0100

    Update Acl.php

--- app/Http/Middleware/Acl.php
+++ app/Http/Middleware/Acl.php
@@ -4,50 +4,34 @@ use Closure;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Routing\Middleware;
 
-use App\Database\Models\Resource;
-use App\Helper\AclHelper;
-
 class Acl implements Middleware {
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $resource;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ( hasResource($request->route()->getName()) )
+        {
+            return $next($request);
+        }
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-  	public function __construct(Guard $auth, Resource $resource)
-	{
-            $this->aclHelper = new AclHelper($auth, $resource);
-  	}
-
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-        public function handle($request, Closure $next)
-	{
-	    if ( $this->aclHelper->checkUserPermissions($request->route()->getName()) )
-	    {
-                return $next($request);
-            }
-            return ($request->ajax()) ? response('Unauthorized.', 401) : redirect()->guest('auth/login');    
-	}
+        return ($request->ajax()) ? response('Unauthorized.', 401) : redirect()->back();    
+    }
 
 }
