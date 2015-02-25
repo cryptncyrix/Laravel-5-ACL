9692936a7f658063340d267eefe10e3c6a696747
cryptncyrix <cyrix@t-online.de>
2015-02-25 22:52:19 +0100

    Update all.blade.php

--- resources/views/user/all.blade.php
+++ resources/views/user/all.blade.php
@@ -13,27 +13,29 @@
                                     <tr>
                                         <th class="text-center">Id</th>
                                         <th class="text-center">Name</th>
-                                        @if(in_array('acl.listUserResources', $acl) || in_array('acl.listUserRoles', $acl))
+                                        @if(hasResource('acl.listUserResources') || hasResource('acl.listUserRoles'))
                                             <th class="text-center">Bearbeiten</th>
                                         @endif
                                         
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    
                                     @foreach($dbUser as $value)
                                         <tr class="text-center">
                                             <td>{!! $value->id !!}</td>
                                             <td>{!! $value->name !!}</td>                                           
                                                 <td>
-                                                    @if(in_array('acl.listUserResources', $acl))
-                                                        <a href="{!! route('acl.listUserResources', $value->id) !!}">User Rechte hinzufÃ¼gen </a>
+                                                    @if(hasResource('acl.listUserResources'))
+                                                        <a href="{!! route('acl.listUserResources', $value->id) !!}">User Rechte hinzufügen </a>
                                                     @endif
-                                                    @if(in_array('acl.listUserRoles', $acl))
-                                                        <a href="{!! route('acl.listUserRoles', $value->id) !!}">User Rollen hinzufÃ¼gen </a>
+                                                    @if(hasResource('acl.listUserRoles'))
+                                                        <a href="{!! route('acl.listUserRoles', $value->id) !!}">User Rollen hinzufügen </a>
                                                     @endif
                                                 </td>                                            
                                         </tr>
                                     @endforeach
+                                    
                                 </tbody>
                             </table> 
                         {!! $dbUser->render() !!}
