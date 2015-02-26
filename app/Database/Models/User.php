8a0bbd03ebca17abf08f1b7ba9749de58011153f
cryptncyrix <cyrix@t-online.de>
2015-02-26 14:37:34 +0100

    Update User.php

--- app/Database/Models/User.php
+++ app/Database/Models/User.php
@@ -1,15 +1,8 @@
 <?php namespace App\Database\Models;
 
-use Hash;
-use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
-
-    use Authenticatable, CanResetPassword;
+class User extends Model {
 
     /**
      * The database table used by the model.
