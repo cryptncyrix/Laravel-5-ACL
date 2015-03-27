744df63ccbff3e4bc0e043d0bfcb5c34ab27c8fb
cryptncyrix <cyrix@t-online.de>
2015-03-27 12:05:09 +0100

    Update helpers.php

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -12,3 +12,23 @@ function hasResource($name)
 {
     return app('aclhelper')->hasResource($name);
 }
+^
+/**
+ * @param string $fallBack
+ * @return mixed
+ */
+function getSessionBackLink($fallBack)
+{
+    if(array_key_exists('back', \Session::all())){
+        return \Session::pull('back');
+    }
+    return $fallBack;
+}
+
+/**
+ * @param string $param
+ * @return mixed
+ */
+function setSessionBackLink($param) {
+    return \Session::put('back', $param);
+}
